79708b56f63a2d0556e440ed06961bf00127daca
WendyOmollo <omollow280@gmail.com>
2019-09-22 21:59:57 +0300

    Add getters and setters

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,39 @@
 package models;
 
 public class Department {
+
+    private String name;
+    private String description;
+    private int employees;
+    private int id;
+
+    public Department(String name,String description,int employees){
+        this.name=name;
+        this.description = description;
+        this.employees=employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(int employees) {
+        this.employees = employees;
+    }
 }
