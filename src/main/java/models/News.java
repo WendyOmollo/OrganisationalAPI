8643e0947326cd83a81fbd6901e1520c7cadc1b9
WendyOmollo <omollow280@gmail.com>
2019-09-22 22:08:53 +0300

    Add getters,setters,equals and hashCode

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,45 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
+    private String title;
+    private String details;
+    private int id;
+
+    public News(String title, String details) {
+        this.title = title;
+        this.details = details;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return id == news.id &&
+                Objects.equals(getTitle(), news.getTitle()) &&
+                Objects.equals(getDetails(), news.getDetails());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getDetails(), id);
+    }
 }
