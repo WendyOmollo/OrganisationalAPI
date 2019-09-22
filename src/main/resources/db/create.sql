a4e984a3ce859895ce9c90c82b5a08486efe99e4
WendyOmollo <omollow280@gmail.com>
2019-09-23 00:09:12 +0300

    Create a table for classified news

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS departments(
 id int PRIMARY KEY auto_increment,
 name VARCHAR,
 description VARCHAR,
-employees INTEGER
+employees INTEGER,
+classfiedNews_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS news(
@@ -17,5 +18,14 @@ CREATE TABLE IF NOT EXISTS employees(
 id int PRIMARY KEY auto_increment,
 name VARCHAR,
 department_id INTEGER,
-position VARCHAR
+position VARCHAR,
+classifiedNews_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS classified_news(
+id int PRIMARY KEY auto_increment,
+title VARCHAR,
+details VARCHAR,
+department_id INTEGER,
+employee_id INTEGER,
 );
